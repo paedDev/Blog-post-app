3ce578c5c5e8c9ba8634a39c5d22c9b45c9c35c7
paedDev <jannoelpaed27@gmail.com>
2024-10-28 22:16:05 +0800

    Added search, view single news and post route

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,7 +1,7 @@
 import express from "express";
 import axios from "axios";
 const newsRouter = express.Router();
-const API_URL = "https://raddy.dev/wp-json/wp/v2/posts"
+const API_URL = "https://raddy.dev/wp-json/wp/v2/posts/"
 newsRouter.get("/", async (req,res) => {
     try {
         const result = await axios.get(`${API_URL}`)
@@ -11,14 +11,71 @@ newsRouter.get("/", async (req,res) => {
         
     } catch (error) {
         if(error.response){
-            console.log(err.response.data);
-            console.log(err.response.status);
-            console.log(err.response.headers);
+            res.render("news", {
+                articles : null
+            })
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
         }else{
-            console.err("Error", err.message);
+             res.render("news", {
+                articles : null
+            })
+            console.error("Error", error.message);
             
         }
     }
 })
 
+
+newsRouter.get("/:id", async (req,res) => {
+    let articleID = req.params.id
+    try {
+        const result = await axios.get(`${API_URL}${articleID}`)
+        res.render("newsSingle", {
+            article : result.data
+        })
+        
+    } catch (error) {
+        if(error.response){
+            res.render("newsSingle", {
+                article : null
+            })
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        }else{
+             res.render("newsSingle", {
+                article : null
+            })
+            console.error("Error", error.message);
+            
+        }
+    }
+})
+newsRouter.post("/", async (req,res) => {
+    let search = req.body.search;
+    try {
+        const result = await axios.get(`https://raddy.dev/wp-json/wp/v2/posts?search=${search}`)
+        res.render("newsSearch", {
+            articles : result.data
+        })
+        
+    } catch (error) {
+        if(error.response){
+            res.render("newsSearch", {
+                articles : null
+            })
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        }else{
+             res.render("newsSearch", {
+                articles: null
+            })
+            console.error("Error", error.message);
+            
+        }
+    }
+})
 export default newsRouter
